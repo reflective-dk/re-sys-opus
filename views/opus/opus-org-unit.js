106515373ccfc1bbf299aca36f17b71603f198a6
Michael Nielsen <michael@reflective.dk>
2021-10-05 13:36:42 +0000

    opus-org-unit view + test

--- views/opus/opus-org-unit.js
+++ views/opus/opus-org-unit.js
@@ -1,30 +1,122 @@
 define([
-    'webix', 'common/$$', 'common/lodash', 'common/webix-component-helper', './snippets/entity'
-], function(webix, $$, _, wch, entity) {
+    'webix', 'common/$$', 'common/lodash', 'common/webix-component-helper', 'views/common/snippets/active-periode'
+], function(webix, $$, _, wch, activePeriode) {
   webix.protoUI({
-    name: "ro-unit",
+    name: "opus-org-unit",
     $init: function(config) {
       this.$view.className += " ro-unit";
       config = wch.$init.apply(this, [config, {id: "40864bd7-2ac2-468d-80a5-68aefa018524", name: "opus-org-unit"}]);
-      config.mandatory = {name: true};
+      config.readonly = true;
       config.elements = [{
         view:"fieldset",
         label:"OPUS Enhed",
         body: {
-          rows: [
-          entity(config),
+          rows: [{
+            view: "text",
+            readonly: true,
+            name: "orgUnitId",
+            label: "Id"
+          },{
+            view: "text",
+            readonly: true,
+            name: "client",
+            label: "Klient"
+          },{
+            view: "fieldset",
+            label: "Start/Slut interval",
+            hidden: config.exclude["startDate"] && config.exclude["endDate"],
+            body: {
+              margin: 0,
+              cols: [{
+                view: "reDatepicker",
+                label: "Start",
+                labelWidth: 80,
+                readonly: true,
+                name: "startDate"
+              },{
+                width: 10
+              },{
+                view: "reDatepicker",
+                readonly: true,
+                label: "Slut",
+                labelWidth: 80,
+                name: "endDate"
+              }]
+            }
+          },
+          activePeriode(config),
           {
-            view: "checkbox",
-            name: "intraNote",
-            labelWidt: 0,
-            labelRight: "IntraNote bund",
-            tooltip: "Nedereste enhed vist i IntraNote. Medarbejdere under flyttes op til denne enhed"
+            view: "text",
+            readonly: true,
+            name: "shortName",
+            label: "Kort navn"
+          },{
+            view: "text",
+            readonly: true,
+            name: "longName",
+            label: "Langt navn"
+          },{
+            view: "text",
+            readonly: true,
+            name: "street",
+            label: "Gade"
+          },{
+            view: "text",
+            readonly: true,
+            name: "zipCode",
+            label: "Postnr"
+          },{
+            view: "text",
+            readonly: true,
+            name: "city",
+            label: "By"
+          },{
+            view: "text",
+            readonly: true,
+            name: "phoneNumber",
+            label: "Telefonnummer"
+          },{
+            view: "text",
+            readonly: true,
+            name: "eanNr",
+            label: "EAN"
+          },{
+            view: "text",
+            readonly: true,
+            name: "seNr",
+            label: "SE Nr"
+          },{
+            view: "text",
+            readonly: true,
+            name: "pNr",
+            label: "P Nr"
+          },{
+            view: "text",
+            readonly: true,
+            name: "costCenter",
+            label: "Omkostningssted"
+          },{
+            view: "text",
+            readonly: true,
+            name: "orgType",
+            label: "Type"
+          },{
+            view: "text",
+            readonly: true,
+            name: "orgTypeTxt",
+            label: "Type tekst"
           },{
             view: "checkbox",
-            name: "stsOrg",
+            name: "notAdministrativeUnit",
             labelWidt: 0,
-            labelRight: "Undtaget fra STS Organisation",
-            tooltip: "Undtag enhed fra opbygning af STS Organisation"
+            labelRight: "Undtaget enhed",
+            tooltip: "Undtag enhed fra opbygning af administrativ organisation"
+          },{
+            view: "checkbox",
+            name: "noUpdateOfAdministrativeParent",
+            labelWidt: 0,
+            labelRight: "Valgfri overenhed",
+            tooltip: "Medtag ikke overenhed ved opbygning af administrativ organisation"
           }]
         }
       }];
